31cc2ef030e30cdc472eb21655b649876bf5cf02
Laystoun <timureubov655@gmail.com>
2026-03-13 19:06:22 +0300

    TCP соединение, пока что просто соединение без удержания.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
-namespace ip = boost::asio::ip;
-namespace asio = boost::asio;
-namespace system = boost::system;
+namespace bip = boost::asio::ip;
+namespace basio = boost::asio;
+namespace bsystem = boost::system;
 
-class Server {
+class Server
+{
+public:
+    Server(basio::io_context& context, int port) : socket{context}, acceptor{context, bip::tcp::endpoint(bip::tcp::v4(), port)} 
+    {
+        
+    }
+
+    void asc_acceptor()
+    {
+        acceptor.async_accept(socket, [&](boost::system::error_code errorcode)
+        {   
+            if (!errorcode) {
+                std::cout << "Connect success...\n";
+                asc_acceptor();
+            } else {
+                std::cout << errorcode.message();
+            }
+        });
+    }
 
 private:
-    ip::tcp::socket socket;
+    int port;
+    bip::tcp::socket socket;
+    bip::tcp::acceptor acceptor;
 };
 
-int main() {
-    
+int main()  
+{
+    basio::io_context context;
+    int port = 4051;
+    Server server(context, port);
+    server.asc_acceptor();
+
+    context.run();
 }
