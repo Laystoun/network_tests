a5958625ac8aecf9c17361e31d043808aba7b25f
Laystoun <timureubov655@gmail.com>
2026-03-13 17:56:59 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+namespace ip = boost::asio::ip;
+namespace asio = boost::asio;
+namespace system = boost::system;
+
+class Server {
+
+private:
+    ip::tcp::socket socket;
+};
+
+int main() {
+    
+}
