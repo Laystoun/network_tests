9d570455d2bbdc16125d4de718458aae6c1188c2
Laystoun <timureubov655@gmail.com>
2026-03-13 21:30:16 +0300

    Асинхронный TCP-сервер, позволяет устанавливать соединение, инициализировать сессию и отправлять сообщение на сервер после чего автоматически сессия удаляется

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <boost/asio.hpp>
 #include <iostream>
+#include <memory>
 
 namespace bip = boost::asio::ip;
 namespace basio = boost::asio;
 namespace bsystem = boost::system;
+
+class Session: public std::enable_shared_from_this<Session> {
+public:
+    
+    Session(bip::tcp::socket&& mv_socket) : session_socket(std::move(mv_socket)) {
+        std::cout << "Session initialized..." << std::endl;
+    }
+
+    ~Session() {
+        std::cout << "Session destroy" << std::endl;
+    }
+
+    void createSession() {
+        basio::async_read_until(
+            session_socket,
+            buffer,
+            "\n",
+            [this_session = shared_from_this()](bsystem::error_code errorcode, std::size_t bytes_transf)
+            {
+                if (!errorcode) {
+                    std::cout << &this_session->buffer;
+                }
+            }
+        );
+    }
+
+private:
+    bip::tcp::socket session_socket;
+    basio::streambuf buffer;
+};
 
 class Server
 {
@@ -18,7 +49,9 @@ public:
         acceptor.async_accept(socket, [&](boost::system::error_code errorcode)
         {   
             if (!errorcode) {
-                std::cout << "Connect success...\n";
+                std::shared_ptr<Session> nsession = std::make_shared<Session>(std::move(socket));
+                nsession->createSession();
+        
                 asc_acceptor();
             } else {
                 std::cout << errorcode.message();
